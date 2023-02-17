7842a0327c5f16071f256f546ea29d736dc9e49f
Beqqer <dimos.as@mail.ru>
2023-02-17 15:41:03 +0300

    init auth/order

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,13 @@
 import { addBannerEventListeners } from '../scripts/modules/index.js'
 import { createProductCard } from '../scripts/components/index.js'
-import { createAuthorizationWindow } from './components/authorizationWindow.js'
+// import { createAuthorizationWindow } from './components/authorizationWindow.js'
+import { createOrderingModal } from './components/ordering.js'
 
 export function initApp () {
   createProductCard()
   addBannerEventListeners()
-  createAuthorizationWindow()
+  // createAuthorizationWindow()
+  createOrderingModal()
   // getLocalStorage()
   // setLocalStorage()
 }
--- src/scripts/components/authorizationWindow.js
+++ src/scripts/components/authorizationWindow.js
@@ -1,96 +1,97 @@
-export function createAuthorizationWindow() {
-    const modalOverlay = document.createElement('div')
-    modalOverlay.className = 'modal-overlay'
-    document.querySelector('.container').append(modalOverlay)
+// export function createAuthorizationWindow() {
+//     const modalOverlay = document.createElement('div')
+//     modalOverlay.className = 'modal-overlay'
+//     document.querySelector('.container').append(modalOverlay)
 
 
-    const modal = document.createElement('div')
-    modal.className = 'modal'
-    modalOverlay.append(modal)
+//     const modal = document.createElement('div')
+//     modal.className = 'modal'
+//     modalOverlay.append(modal)
 
-    const modalSignIn = document.createElement('div')
-    modalSignIn.className = 'modal__sign-in'
-    modal.append(modalSignIn)
+//     const modalSignIn = document.createElement('div')
+//     modalSignIn.className = 'modal__sign-in'
+//     modal.append(modalSignIn)
 
-    const modalTitle = document.createElement('p')
-    modalTitle.className = 'modal__title-text'
-    modalTitle.innerText = 'Войти или создать'
-    modalSignIn.append(modalTitle)
+//     const modalTitle = document.createElement('h2')
+//     modalTitle.className = 'modal__title-text'
+//     modalTitle.innerHTML = `Войти или создать <br> профиль`
+//     modalSignIn.append(modalTitle)
 
-    const modalSubTitle = document.createElement('p')
-    modalSubTitle.className = 'modal__title-text modal__subtitle-text'
-    modalSubTitle.innerText = 'профиль'
-    modalTitle.append(modalSubTitle)
+//     // const btnClose = document.createElement('button')
+//     // btnClose.className = 'close-modal'
+//     // btnClose.type = 'button'
+//     // modalSignIn.append(btnClose)
 
+//     // const btnCross = document.createElement('span')
+//     // btnCross.className = 'close-modal-cross'
+//     // btnClose.append(btnCross)
 
-    // const btnClose = document.createElement('button')
-    // btnClose.className = 'close-modal'
-    // btnClose.type = 'button'
-    // modalSignIn.append(btnClose)
+//     const formSignIn = document.createElement('form')
+//     formSignIn.className = 'modul__form-sign-in'
+//     formSignIn.action = "#"
+//     modalSignIn.append(formSignIn)
 
-    // const btnCross = document.createElement('span')
-    // btnCross.className = 'close-modal-cross'
-    // btnClose.append(btnCross)
+//     const loginSignIn = document.createElement('input')
+//     loginSignIn.className = 'modal__input-sign-in'
+//     loginSignIn.placeholder = 'Логин'
+//     loginSignIn.type = 'text'
+//     loginLogIn.type = 'email'
+//     formSignIn.append(loginSignIn)
 
-    const formSignIn = document.createElement('form')
-    formSignIn.className = 'modul__form-sign-in'
-    modalSignIn.append(formSignIn)
+//     const passwordSignIn = document.createElement('input')
+//     passwordSignIn.className = 'modal__input-sign-in'
+//     passwordSignIn.placeholder = 'Пароль'
+//     passwordSignIn.type = 'text'
+//     passwordSignIn.type = 'password'
+//     formSignIn.append(passwordSignIn)
 
-    const loginSignIn = document.createElement('input')
-    loginSignIn.className = 'modal__input-sign-in'
-    loginSignIn.placeholder = 'Логин'
-    loginSignIn.type = 'email'
-    formSignIn.append(loginSignIn)
-
-    const passwordSignIn = document.createElement('input')
-    passwordSignIn.className = 'modal__input-sign-in'
-    passwordSignIn.placeholder = 'Пароль'
-    passwordSignIn.type = 'password'
-    formSignIn.append(passwordSignIn)
-
-    const btnAuth = document.createElement('button')
-    btnAuth.className = 'modal__btn-auth'
-    btnAuth.innerText = 'Войти'
-    btnAuth.type = 'button'
-    modalSignIn.append(btnAuth)
+//     const btnAuth = document.createElement('button')
+//     btnAuth.className = 'modal__btn-auth'
+//     btnAuth.innerText = 'Войти'
+//     btnAuth.type = 'button'
+//     modalSignIn.append(btnAuth)
 
 
-    const modalLine = document.createElement('div')
-    modalLine.className = 'line'
-    modal.append(modalLine)
+//     const modalLine = document.createElement('div')
+//     modalLine.className = 'line'
+//     modal.append(modalLine)
 
 
 
-    const modalLogIn = document.createElement('div')
-    modalLogIn.className = 'modal-down__log-in'
-    modal.append(modalLogIn)
+//     const modalLogIn = document.createElement('div')
+//     modalLogIn.className = 'modal-down__log-in'
+//     modal.append(modalLogIn)
 
-    const formLogIn = document.createElement('form')
-    formLogIn.className = 'modal-down__form-log-in'
-    modalLogIn.append(formLogIn)
+//     const formLogIn = document.createElement('form')
+//     formLogIn.className = 'modal-down__form-log-in'
+//     formLogIn.action = '#'
+//     modalLogIn.append(formLogIn)
 
-    const loginLogIn = document.createElement('input')
-    loginLogIn.className = 'modal-down__input-log-in'
-    loginLogIn.placeholder = 'Логин' // не отображает и остальные тоже
-    loginLogIn.type = 'email'
-    formLogIn.append(loginLogIn)
+//     const loginLogIn = document.createElement('input')
+//     loginLogIn.className = 'modal-down__input-log-in'
+//     loginLogIn.placeholder = 'Логин'
+//     loginLogIn.type = 'text'
+//     loginLogIn.type = 'email'
+//     formLogIn.append(loginLogIn)
 
-    const passwordLogIn = document.createElement('input')
-    passwordLogIn.className = 'modal-down__input-log-in'
-    passwordLogIn.placeholder = 'Пароль'
-    passwordLogIn.type = 'password'
-    formLogIn.append(passwordLogIn)
+//     const passwordLogIn = document.createElement('input')
+//     passwordLogIn.className = 'modal-down__input-log-in'
+//     passwordLogIn.placeholder = 'Пароль'
+//     passwordLogIn.type = 'text'
+//     passwordLogIn.type = 'password'
+//     formLogIn.append(passwordLogIn)
 
-    const againPassword = document.createElement('input')
-    againPassword.className = 'modal-down__input-log-in'
-    againPassword.placeholder = 'Повторите пароль'
-    againPassword.type = 'password'
-    formLogIn.append(againPassword)
+//     const againPassword = document.createElement('input')
+//     againPassword.className = 'modal-down__input-log-in'
+//     againPassword.placeholder = 'Повторите пароль'
+//     againPassword.type = 'text'
+//     againPassword.type = 'password'
+//     formLogIn.append(againPassword)
 
-    const btnLogIn = document.createElement('button')
-    btnLogIn.className = 'modal-down__btn-log-in'
-    btnLogIn.innerText = 'Создать профиль'
-    btnLogIn.type = 'button'
-    modalLogIn.append(btnLogIn)
+//     const btnLogIn = document.createElement('button')
+//     btnLogIn.className = 'modal-down__btn-log-in'
+//     btnLogIn.innerText = 'Создать профиль'
+//     btnLogIn.type = 'button'
+//     modalLogIn.append(btnLogIn)
 
-}
+// }
