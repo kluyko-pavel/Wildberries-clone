12973118b10de48816e22fbd2109fba9ed3e816b
Yegor Volovich <yegorvolovich98@gmail.com>
2023-02-19 15:44:03 +0300

    basketCard created

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,7 +9,13 @@ import {
   getCurrentProduct,
   showCards
 } from '../scripts/modules/index.js'
-import { createPhoneBtn, createTopScrollBtn, createShowMoreBtn, createBasket } from '../scripts/components/index.js'
+import {
+  createPhoneBtn,
+  createTopScrollBtn,
+  createShowMoreBtn,
+  createBasket,
+  createBasketCard
+} from '../scripts/components/index.js'
 
 export function initApp () {
   const basket = createBasket()
@@ -38,4 +44,9 @@ export function initApp () {
   showCards(showMoreBtn)
   showMoreBtn.addEventListener('click', () => handlerShowMore(showMoreBtn))
   window.addEventListener('resize', () => showCards(showMoreBtn))
+
+  createBasketCard('https://basket-05.wb.ru/vol774/part77426/77426595/images/c246x328/1.webp', '28', '17', 'BMCase', 'Силиконовый чехол картхолдер для карт, прозрачный кейс бампер на Apple iPhone 7,8', '1'
+  )
+  createBasketCard('https://basket-01.wb.ru/vol128/part12801/12801489/images/c246x328/1.webp', '120', '84', 'United Colors of Benetton', 'Футболка хлопок однотонная', '1'
+  )
 }
